a98dd7efdd5bb4e468ab747a61666225a0d7860c
Shahbaz <shahbaz_ali@outlook.in>
2022-03-03 00:44:51 +0530

    linked list representation of binary tree, node creation using functions

--- linked_rep_bin_tree.c
+++ linked_rep_bin_tree.c
@@ -7,24 +7,39 @@ struct node{
     struct node* right;
 };
 
+struct node* newNode(int data){
+    struct node* p; // creating a node pointer
+    p = (struct node*) malloc(sizeof(struct node)); // allocating memory for the node
+    p->data = data; // assigning the data to the node
+    p->left = NULL; // assigning the left child to NULL
+    p->right =NULL; // assigning the right child to NULL
+    return p;
+}
+
 int main (){
-    // creating parent node
-    struct node *p;
-    p = (struct node *) malloc(sizeof(struct node));
-    p->left = NULL;
-    p->right = NULL;
+    // creating parent node and assigning the data using function
+    struct node* p = newNode(1);
 
-    // creating left child node
-    struct node *p1;
-    p1 = (struct node *) malloc(sizeof(struct node));
-    p1->left = NULL;
-    p1->right = NULL;
+    // // creating left child node without function
+    // struct node *p1;
+    // p1 = (struct node *) malloc(sizeof(struct node));
+    // p1->left = NULL;
+    // p1->right = NULL;
 
-    // creating right child node
-    struct node *p2;
-    p2 = (struct node *) malloc(sizeof(struct node));
-    p2->left = NULL;
-    p2->right = NULL;
+    // // creating right child node without function
+    // struct node *p2;
+    // p2 = (struct node *) malloc(sizeof(struct node));
+    // p2->left = NULL;
+    // p2->right = NULL;
+
+    // creating two child nodes using function
+    struct node *p1 = newNode(2);
+    struct node *p2 = newNode(3);
+
+    // assigning the left child to the parent node
+    p->left = p1;
+    // assigning the right child to the parent node
+    p->right = p2;
 
     return 0;
 }
